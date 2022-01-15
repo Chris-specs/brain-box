0c77d4f3b02db926d6262b3d0ab6e393ffcfa6a9
Christian Sanchez <christian.s.molina@gmail.com>
2022-01-15 13:30:23 -0600

    Quick fix

--- src/context/UserContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-
-export const UserContext = createContext(null);
-
-export const useUser = () => {
-    const user = useContext(UserContext);
-    return { user };
-};
