10c8017c3d90522a23df5ec0cad787f85eb8aab4
Christian Sanchez <christian.s.molina@gmail.com>
2022-01-16 23:10:10 -0600

    Switch task to idea component

--- src/components/Idea.jsx
+++ src/components/Idea.jsx
@@ -8,9 +8,9 @@ const Idea = ({ task }) => {
         <>
             <li className='w-[calc(50%-0.5rem)] h-40 border border-gray-300 rounded-lg overflow-hidden'>
                 <Link href={`/brain/${_id}`}>
-                    <a className='w-full h-full flex flex-col p-3'>
+                    <a className='w-full h-full flex flex-col p-3 overflow-hidden'>
                         <span className='text-sm text-gray-900 font-medium mb-2'>{name}</span>
-                        <span className='text-xs text-gray-900'>{content}</span>
+                        <span className='text-xs text-gray-900 overflow-hidden'>{content}</span>
                     </a>
                 </Link>
             </li>
